29b56fd7de4ae9cbd3ee4aae77362043d9384154
Jon Stapleton <jstapleton@harrisonburg.k12.va.us>
2019-09-18 14:31:39 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,20 +1,5 @@
-var score = 1; // Create a variable to store the score in
+var whatever = new Button("I don't know, mister", btn);
 
-var title = new Title('Welcome to the Game'); // Create a title element
-var button = new Button('hello', btnPress);   // Create a button element
-var hidden = new Button('Boo!');
-hidden.hide();
-var scr = new Text(score);                    // Create a text element
-
-setInterval(btnPress, 1000);   // Set up a loop
-
-function btnPress() {
-  score++;         // Increase the score
-  scr.edit(score); // Update the page with the new score
-  if(score > 10) {
-    hidden.show();
-  }
-  if(score > 15) {
-    hidden.hide(500);
-  }
+function btn() {
+  whatever.edit(randomID())
 }
