0bc447cb07187f20ef89bb28ab220bf53915a089
Jon Stapleton <jstapleton@harrisonburg.k12.va.us>
2019-09-24 15:10:16 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,14 +1,12 @@
-var whatever = new Button("I don't know, mister", btn);
-
-var title = new Title('Welcome to the Game'); // Create a title element
-var button = new Button('hello', btnPress);   // Create a button element
-var hidden = new Button('Boo!');
-hidden.hide();
-var scr = new Text(score);                    // Create a text element
+var newButton = new Button('My Button', click);
 
 setInterval(btnPress, 1000);   // Set up a loop
 
 function btnPress() {
   score++;
   scr.edit(score);
+}
+
+function click() {
+  scr.edit('Clicked!')
 }
